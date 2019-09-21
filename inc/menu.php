91a26e793481868da6838b8a5d95d04e42c03c4a
joshwolk <joshuawolkoff@gmail.com>
2019-09-21 15:01:04 -0400

    Created new basic styling

--- inc/menu.php
+++ inc/menu.php
@@ -1,6 +1,6 @@
 <!-- Eyebrow Menu. php include statement pasted on top of each page within the sub-sites -->
 
-<nav>
+<nav class="menu">
 	<ul>
 		<li><a href="../cmarti21/lab01/index.php">Pancho</a></li>
 		<li><a href="../jhuo3/index.php">Jialin</a></li>
