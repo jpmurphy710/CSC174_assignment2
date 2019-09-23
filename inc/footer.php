6017bc9021607e52ed80cfd750adcf154db68e97
jpmurphy710 <jpmurphy710@gmail.com>
2019-09-23 03:05:48 -0400

    Javascript
    Installed javascript, started footer include

---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+	<footer>
+			CSC 174: Advanced Front-end and Web Design and Development
+	</footer>
+
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="js/menu-highlighter.js"></script>
