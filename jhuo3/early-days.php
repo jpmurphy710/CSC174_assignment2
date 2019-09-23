311e3288fb60495372c4f64c6a2e033712895536
jpmurphy710 <jpmurphy710@gmail.com>
2019-09-23 03:09:20 -0400

    jhuo3 footer include

--- jhuo3/early-days.php
+++ jhuo3/early-days.php
@@ -14,10 +14,7 @@ include "inc/html-top.php";
 		<p>My hometown is in Shanghai, China, but I have moved to San Francisco Bay Area since when I was thirteen years old. I studied at a Christian school in which I skipped my eighth grade and started from high school immediately. During my fantastic four years in high school, I have demonstrated strong interest in visual designs and various categories of art including calligraphy, painting, photography, and etc. With further curiosity, I have begun learning the technological instruments and software, particularly Adobe Photoshop and Blender.</p>
 	</main>
 
-	<footer>CSC 174: Advanced Front-end Web Design and Development</footer>
-
-	<script src="http://code.jquery.com/jquery.js"></script>
-	<script src="js/menu-highlighter.js"></script>
+	<?php include "../inc/footer.php"; ?>
 
 </body>
 
